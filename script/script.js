c04d1afd987aa2978384c7beb683df07a884690b
Aleximuss <109619022+Aleximuss@users.noreply.github.com>
2022-10-16 12:57:28 +0300

    Delete script directory

--- script/script.js
+++
@@ -1,5 +0,0 @@
-$('.question_item > div').on('click', function() {
-    $(this).parent('.question_item').toggleClass('change-item-color');
-    $(this).next('.question_text').slideToggle(200);
-    $(this).children('.question_item_wrapper').children('.item-2').toggleClass('item-2-off');
-})
