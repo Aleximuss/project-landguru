854eb351139f00c86e9d2d336a5dc369708bb55c
Aleximuss <109619022+Aleximuss@users.noreply.github.com>
2022-10-16 12:47:27 +0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,13 +1,3 @@
-$('#play_video').on('click', function (e) {
-    e.preventDefault();
-    $('html, body').animate({scrollTop: $('#about').offset().top}, 500)
-})
-
-$('#open-modal').on('click', function (e) {
-    e.preventDefault();
-    $('html, body').animate({scrollTop: $('#feedback').offset().top}, 500)
-})
-
 $('.question_item > div').on('click', function() {
     $(this).parent('.question_item').toggleClass('change-item-color');
     $(this).next('.question_text').slideToggle(200);
