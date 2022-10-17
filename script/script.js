8dcf03112e0abe90d8e61cbe388b9065b162293f
Aleximuss <109619022+Aleximuss@users.noreply.github.com>
2022-10-18 00:40:29 +0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -3,3 +3,23 @@ $('.question_item > div').on('click', function() {
     $(this).next('.question_text').slideToggle(200);
     $(this).children('.question_item_wrapper').children('.item-2').toggleClass('item-2-off');
 })
+
+$('#home-link').on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop: $('#home').offset().top}, 500)
+})
+
+$('#advertise-link').on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop: $('#pricing').offset().top}, 500)
+})
+
+$('#supports-link').on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop: $('#supports').offset().top}, 500)
+})
+
+$('#contact-link').on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop: $('#contact').offset().top}, 500)
+})
